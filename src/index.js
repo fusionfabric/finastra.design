ca21eabd5918e11cf8669e3cea77c1023e5f9567
frigui dorsaf <dorsafrigui@gmail.com>
2021-02-01 17:28:28 +0100

    mission animation(in progress)

--- src/index.js
+++ src/index.js
@@ -4,10 +4,35 @@ import './styles/index.scss'
 import { gsap } from "gsap";
 import { TextPlugin } from "gsap/TextPlugin";
 
+import 'regenerator-runtime/runtime'
+
 gsap.registerPlugin(TextPlugin);
 const middle = innerWidth / 2;
 
 var tl = gsap.timeline({repeat: -1, repeatDelay: 1});
+
+init();
+
+const sleep = ms => new Promise(res => setTimeout(res, ms));
+
+let run=async function(){
+  show1();
+  hide2();
+  hide3();
+  await sleep(3000);
+  hide1();
+  show2();
+  await sleep(3000);
+  hide2();
+  show3();
+  await sleep(3000);
+}
+
+run();
+window.setInterval(async function(){
+  run();
+},10000);
+
 
 tl.to(".title-emphasis", {y: -100, duration: 0.5}, 1);
 tl.set(".title-emphasis", {text: "change"}, 1.2);
@@ -31,3 +56,68 @@ window.addEventListener("mousemove", e => {
   gsap.to(".img3 ", {x: -(0.080*pos), duration: 1});
   gsap.to(".crop-mark ", {x: -(0.075*pos), duration: 1});
 });
+
+function init(){
+  hide2();
+  hide3();
+  document.getElementsByClassName("mission-text-1")[0].style.setProperty("--content-mission-tag-color", '#56F28C');
+  document.getElementsByClassName("mission-text-2")[0].style.setProperty("--content-mission-tag-color", '#36D4F2');
+  document.getElementsByClassName("mission-text-3")[0].style.setProperty("--content-mission-tag-color", '#E945C1');
+}
+
+function hide1() {
+  document.getElementsByClassName("mission-text-content-1")[0].style.display = "none";
+  document.getElementsByClassName("mission-text-title-1")[0].style.opacity="0.5";
+  document.getElementsByClassName("mission-text-1")[0].style.setProperty("--content-mission-tag-width", '13%');
+}
+
+function hide2() {
+  document.getElementsByClassName("mission-text-content-2")[0].style.display = "none";
+  document.getElementsByClassName("mission-text-title-2")[0].style.opacity="0.5";
+  document.getElementsByClassName("mission-text-2")[0].style.setProperty("--content-mission-tag-width", '13%');
+}
+
+function hide3() {
+  document.getElementsByClassName("mission-text-content-3")[0].style.display = "none";
+  document.getElementsByClassName("mission-text-title-3")[0].style.opacity="0.5";
+  document.getElementsByClassName("mission-text-3")[0].style.setProperty("--content-mission-tag-width", '13%');
+}
+
+function show1() {
+  document.getElementsByClassName("mission-text-content-1")[0].style.display = "block";
+  document.getElementsByClassName("mission-text-title-1")[0].style.opacity="1";
+  document.getElementsByClassName("img-1")["0"].src="./images/home/content/mission/img1.png";
+
+  document.getElementsByClassName("mission-text-1")[0].style.setProperty("--content-mission-tag-width", '20%');
+  document.getElementsByClassName("mission-text-1")[0].style.setProperty("--content-mission-tag-color", '#56F28C');
+
+  document.getElementsByClassName("mission-image")[0].style.setProperty("--content-mission-image-background",'linear-gradient(180deg,#30D897 0%, #56F28C 100%) 0% 0%');
+  document.getElementsByClassName("mission-image")[0].style.setProperty("--content-mission-background", 'url("./images/home/content/mission/icon-arrow.svg") no-repeat');
+
+}
+
+function show2() {
+  document.getElementsByClassName("mission-text-content-2")[0].style.display = "block";
+  document.getElementsByClassName("mission-text-title-2")[0].style.opacity="1";
+  document.getElementsByClassName("img-1")["0"].src="./images/home/content/mission/img2.png";
+
+  document.getElementsByClassName("mission-text-2")[0].style.setProperty("--content-mission-tag-width", '20%');
+  document.getElementsByClassName("mission-text-2")[0].style.setProperty("--content-mission-tag-color", '#36D4F2');
+
+  document.getElementsByClassName("mission-image")[0].style.setProperty("--content-mission-image-background",'linear-gradient(180deg,#36D4F2 0%, #36D4F2 100%) 0% 0%');
+  document.getElementsByClassName("mission-image")[0].style.setProperty("--content-mission-background", 'url("./images/home/content/mission/code.svg") no-repeat');
+}
+
+function show3() {
+  document.getElementsByClassName("mission-text-content-3")[0].style.display = "block";
+  document.getElementsByClassName("mission-text-title-3")[0].style.opacity="1";
+  document.getElementsByClassName("img-1")["0"].src="./images/home/content/mission/img3.png";
+
+  document.getElementsByClassName("mission-text-3")[0].style.setProperty("--content-mission-tag-color", '#E945C1');
+  document.getElementsByClassName("mission-text-3")[0].style.setProperty("--content-mission-tag-width", '20%');
+
+  document.getElementsByClassName("mission-image")[0].style.setProperty("--content-mission-image-background",'linear-gradient(180deg,#E945C1 0%, #E945C1 100%) 0% 0%');
+  document.getElementsByClassName("mission-image")[0].style.setProperty("--content-mission-background", 'url("./images/home/content/mission/pen.svg") no-repeat');
+}
+
+
